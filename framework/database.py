ce1b50a9452c97cfeab47beebac91ad2a59d9e9f
Michael Giardino <michaelgiardino@Michaels-MBP.fios-router.home>
2020-05-19 21:11:51 -0400

    Added comments to database file

--- framework/database.py
+++ framework/database.py
@@ -1,4 +1,3 @@
-import json
 import time
 import logging
 import datetime
@@ -16,11 +15,12 @@ class StoreAccess():
     def __init__(self,monitor_id):
         self.monitor_id = monitor_id
         self.cluster_string = "" #insert connection string here
-        self.database_string = "status" #set the collection as a enviorment variable to last throughout the session
-        self.collection_string = "monitors"
-        self.database_connect(self.collection_string)
+        self.database_string = "" #insert database name here
+        self.collection_string = "" #insert collection name here
+        self.database_connect(self.collection_string) #connect to database
 
     def database_connect(self,collection_string):
+         # attempt to connect to collection corresponding to string passed in
         logging.info("MongoDB Connecting...")
         while True:
             try:
@@ -28,6 +28,7 @@ class StoreAccess():
                 database = client[self.database_string]
                 self.collection = database[collection_string]
             except Exception as e:
+                # retry connecting to the client
                 logging.info(e)
                 logging.info('Error connecting to MongoDB, retrying..')
                 time.sleep(10)
@@ -36,30 +37,35 @@ class StoreAccess():
         logging.info("MongoDB Connection Established")
 
     def retrive_product_collections(self):
+        #return all product collections names
         store_docs = list(self.collection.find())
         product_collections = list(dict.fromkeys([i['productCollectionId'] for i in store_docs]))
         return product_collections
 
     def retrive_store_settings(self):
+        #return settings for store
         return list(self.collection.find({"monitorId":self.monitor_id}))[0]
 
     def log_error(self,error_information,product_identifier = "None"):
+        #log monitor issue in its collection document
         error_message = [{'error':"",'time':str(datetime.datetime.now()),"productIdentifier":product_identifier}]
         self.collection.update({"monitorId": self.monitor_id}, { '$push':{'errorLog': {'$each':error_message}}})
 
     def change_monitor_status(self,status):
+        #turn monitor off or on
         self.collection.updateOne({ "monitorId": self.monitor_id },{"$set": { "enabled": status}})
 
 class ProductAccess():
     def __init__(self,monitor_id):
         self.monitor_id = monitor_id
         self.cluster_string = "" #insert connection string here
-        self.database_string = "products" #set the collection as a enviorment variable to last throughout the session
-        self.store_settings = StoreAccess(monitor_id).retrive_store_settings()
-        self.collection_string = self.store_settings['productCollectionId']
+        self.database_string = "products" #insert database name here
+        self.store_settings = StoreAccess(monitor_id).retrive_store_settings() #retrive store settings based on monitor_id
+        self.collection_string = self.store_settings['productCollectionId'] #set name of collection string equal to collection name specified in settings
         self.database_connect(self.collection_string)
 
     def database_connect(self,collection_string):
+        # attempt to connect to collection corresponding to string passed in
         logging.info("MongoDB Connecting...")
         while True:
             try:
@@ -88,14 +94,17 @@ class ProductAccess():
 
 
     def import_new_products(self,products):
+        ## import passed in products to the database
         for product in products:
             self.add_product(product)
         logging.info("All New Products Successfully Imported")
 
     def change_product_status(self,product,status):
+        ## changed marked status of product
         self.collection.updateOne({ self.store_settings['defaultIdentifier']: product[self.store_settings['defaultIdentifier']] },{"$set": { "enabled": status}})
 
     def add_product(self,product):
+        ## add produc to the database
         product_temp = {"productTitle":None,"productPrice":None,"productUrl":None,"productId":None,"productImageUrl":None,"stockLog":[],"postLog":[],"enabled":True,"manuallyAdded":False}
         product_doc = {**product_temp,**product}
         in_stock = product_doc.pop('inStock', None)
@@ -114,6 +123,7 @@ class ProductAccess():
         return success
 
     def log_product_details(self,identifier_value, product_details):
+        ## log details about product that is already in the database
         for value_key in list(product_details.keys()):
             try:
                 self.collection.update({self.store_settings['defaultIdentifier']: identifier_value},  {"$push": {"log": { value_key : product_details['valueKey'] }}})
@@ -122,7 +132,7 @@ class ProductAccess():
         logging.info("Product Details Successfully Updated in Database")
 
     def log_product_status(self,identifier_value, status):
-        ## log product event given its url ##
+        ## log product event given its url
         product = self.retrive_product(identifier_value)
         if len(product['stockLog']) == 0 or product['stockLog'][-1]['inStock'] != status:
             update_message = [{'inStock':status,'time':time.time(),'monitorId':self.monitor_id}]
@@ -132,6 +142,7 @@ class ProductAccess():
             logging.info("Product status has not changed since previous log")
 
     def log_product_post(self,identifier_value,post_ts):
+        ## log that a product has been posted via slack
         product = self.retrive_product(identifier_value)
         update_message = [post_ts]
         self.collection.update({self.store_settings['defaultIdentifier']: identifier_value}, { '$push':{'postLog': {'$each':update_message}}})
